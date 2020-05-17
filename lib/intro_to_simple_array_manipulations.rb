ccd079be56cee63c72ff85172833cfcc33e679ec
Jodi Croft <jodipruett@gmail.com>
2020-05-17 17:10:13 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,35 @@
+def using_push (array, string)
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  next_color = "violet"
+  updated_array = colors_in_the_rainbow.push(next_color)
+end
+
+def using_unshift(array, string)
+  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  new_neighborhood = "Staten Island"
+  updated_array = bouroughs_in_nyc.unshift(new_neighborhood)
+end
+
+
+def using_pop(array)
+  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  @continents.pop
+end
+
+
+def pop_with_args(array)
+  dog_breeds = ["Great Dane", "Golden Retreiver", "Chihuahua", "Shiba Inu"]
+  @dog_breeds.pop(2)
+end
+
+def using_shift(array)
+  my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Fransisco", "Gabrone", "New York", "Berlin", "London"]
+  @my_favorite_cities.shift
+end
+
+
+
+def shift_with_args(array)
+  ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+  @ice_cream_brands.shift(2)
+end
